680e53c44e72cf982f5248ff7ccda8bbb95084d7
Cedric David <cedric.david@btopenworld.com>
2018-08-25 10:24:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return myAnimal() 
+  return myAnimal()
 }
 
 function add2(n) {

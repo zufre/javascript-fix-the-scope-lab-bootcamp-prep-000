2789fc38cb9531f6d913dc4d5ceaa2bcf2450a8d
Cedric David <cedric.david@btopenworld.com>
2018-08-25 10:28:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,11 @@ function yourAnimal() {
 }
 
 function add2(n) {
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+  
 }
 
 var funkyFunction = function() {
